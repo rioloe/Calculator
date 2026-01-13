d7e4b9e84f45704d7c71bd313ae3fd8048a2543a
rioloe <rioloelux@gmail.com>
2026-01-13 20:14:55 +0900

    App 역할 정리

--- src/Calculator/App.java
+++ src/Calculator/App.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        // Scanner 객체 소환
+        // Scanner 객체 생성
         Scanner sc = new Scanner(System.in);
+
+        // Calculator 객체 생성
+        Calculator calculator = new Calculator();
 
         // 반복
         while (true) {
 
-            // 양의 정수 입력 받기
+            // 정수 입력 받기
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int num1 = sc.nextInt();
             System.out.print("두 번쨰 숫자를 입력하세요: ");
@@ -21,30 +24,10 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.next().charAt(0);
 
-            // 사칙 연산 진행
-            int result = 0;
+            // Calculator에게 계산 요청
+            // 계산결과 반환하여 result 변수에 저장
+            int result = calculator.calculate(num1, num2, operator);
 
-            switch (operator) {
-                case '+':
-                    result = num1 + num2;
-                    break;
-                case '-':
-                    result = num1 - num2;
-                    break;
-                case '*':
-                    result = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("0으로 나눌 수 없음");
-                        continue;
-                    }
-                    result = num1 / num2;
-                    break;
-                default:
-                    System.out.println("잘못된 연산자");
-                    continue;
-            }
             // 연산 결과 출력
             System.out.println("결과: " + result);
 
@@ -56,7 +39,6 @@ public class App {
                 System.out.println("프로그램을 종료");
                 break;
             }
-
 
         }
 
